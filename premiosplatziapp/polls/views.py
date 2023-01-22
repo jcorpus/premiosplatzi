12d7a23a54ae527acaa20bb421b947e16e63cef0
jcorpus <doombakuryo@gmail.com>
2023-01-22 11:47:05 -0500

    ultimo

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 from django.http import HttpResponse 
+from .models import Question
 
-
+#vista index
 def index(request):
-    return HttpResponse("hELLO wORD")
+    latest_question_list = Question.objects.all()
+    return render(request,"polls/index.html",{
+        "latest_question_list": latest_question_list
+        })
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    #return HttpResponse(f"Estas viendo la pregunta numero {question_id} ")
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
     
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id} ")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero {question_id} ")
